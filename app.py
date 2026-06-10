43d0c8aa493809bfc9071f2099f7ab4092586342
sravanthi9392-hub <polamsettiramu@gmail.com>
2026-06-10 12:23:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,6 +118,8 @@ def home():
 # ----------------------------------
 # Run Application
 # ----------------------------------
-if __name__ == "__main__":
+import os
 
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
